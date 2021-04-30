3e35d431be3807aa006ddb8f53e50ebf0f08caab
LeagueLugas <48398942+LeagueLugas@users.noreply.github.com>
2021-04-30 11:39:02 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -55,7 +55,7 @@ const movePage = (url) => {
 const request = axios.create({
     baseURL: 'https://api.github.com/repos/LeagueLugas/LeagueLugas.github.io/contents',
     timeout: 1000,
-    headers: {'Authorization': 'token ${{secrets.GH_TOKEN}}'}
+    headers: {'Authorization': 'token ${{ secrets.GH_TOKEN }}'}
 });
 const sectionRequest = (section, limitFrom, limitTo) => {
     request.get('post/' + section).then(data => {
